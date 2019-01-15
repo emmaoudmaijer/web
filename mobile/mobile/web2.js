434013e2f417cb5800f7805251b1665bcbd8742e
Emma Oudmaijer <emma.oudmaijer@hva.nl>
2019-01-15 14:50:05 +0100

    project

---
+++ mobile/mobile/web2.js
@@ -0,0 +1,28 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "500px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+/*var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 2000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
+var likeCounter = 0;
+
+$("a").on("click", function (e) {
+            e.preventDefault();
+*/
